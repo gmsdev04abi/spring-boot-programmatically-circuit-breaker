2de9e4695e26addf1187ff0421c5a46a47a211a0
Guilherme Maciel <guilherme.maciel-ext@ab-inbev.com>
2025-07-09 19:42:52 -0300

    Fix errors

--- src/main/kotlin/br/com/gmsdev04abi/springbootprogramaticallycircuirtbreaker/config/CircuitBreakerConfig.kt
+++ src/main/kotlin/br/com/gmsdev04abi/springbootprogramaticallycircuirtbreaker/config/CircuitBreakerConfig.kt
@@ -1,23 +1,23 @@
-package br.com.gmsdev04abi.springbootprogramaticallycircuirtbreaker.config
+    package br.com.gmsdev04abi.springbootprogramaticallycircuirtbreaker.config
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import java.time.Duration
+    import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+    import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry
+    import org.springframework.context.annotation.Bean
+    import org.springframework.context.annotation.Configuration
+    import java.time.Duration
 
-@Configuration
-class CircuitBreakerConfig {
+    @Configuration
+    class CircuitBreakerConfig {
 
-    @Bean
-    fun circuitBreakerRegistry(): CircuitBreakerRegistry {
-        val config = CircuitBreakerConfig.custom()
-            .failureRateThreshold(50f)
-            .waitDurationInOpenState(Duration.ofSeconds(5))
-            .slidingWindowSize(2)
-            .permittedNumberOfCallsInHalfOpenState(1)
-            .build()
+        @Bean
+        fun circuitBreakerRegistry(): CircuitBreakerRegistry {
+            val config = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50f)
+                .waitDurationInOpenState(Duration.ofSeconds(5))
+                .slidingWindowSize(2)
+                .permittedNumberOfCallsInHalfOpenState(1)
+                .build()
 
-        return CircuitBreakerRegistry.of(config)
+            return CircuitBreakerRegistry.of(config)
+        }
     }
-}
